22b2c388376aa7b266f2fdff74cc716a8af94a47
tomlnd <laine.tomi01@gmail.com>
2025-10-18 00:34:20 +0300

    Add postfix function

--- SHA256.h
+++ SHA256.h
@@ -4,14 +4,15 @@
 #include "Extender.h"
 #include <openssl/sha.h>
 
-class SHA256ex: public Extender
+class SHA256ex : public Extender
 {
 public:
-	SHA256ex();
-	virtual ~SHA256ex();
-	int GenerateSignature(vector<unsigned char> key, vector<unsigned char> message, unsigned char ** signature);
-	bool ValidateSignature(vector<unsigned char> key, vector<unsigned char> message, unsigned char * signature);
-	vector<unsigned char> * GenerateStretchedData(vector<unsigned char> originalMessage, int keylength, unsigned char * hash, vector<unsigned char> added, unsigned char ** newSig);
+  SHA256ex();
+  virtual ~SHA256ex();
+  int GenerateSignature(vector<unsigned char> key, vector<unsigned char> message, unsigned char **signature);
+  bool ValidateSignature(vector<unsigned char> key, vector<unsigned char> message, unsigned char *signature);
+  vector<unsigned char> *GenerateStretchedData(vector<unsigned char> originalMessage, int keylength, unsigned char *hash, vector<unsigned char> added, unsigned char **newSig);
+  vector<unsigned char> *GenerateStretchedData(vector<unsigned char> originalMessage, int keylength, unsigned char *hash, vector<unsigned char> added, vector<unsigned char> postfix, unsigned char **newSig);
 };
 
 #endif
